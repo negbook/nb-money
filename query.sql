7dab0094e43f0a1809d1f596f204bd3d9d731441
negbook <negbook@gmail.com>
2022-06-19 03:25:30 +0800

    Add files via upload

--- query.sql
+++ query.sql
@@ -1,9 +1,7 @@
-CREATE TABLE `money` (
-	`license` VARCHAR(200) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
-	`cash` INT(200) NULL DEFAULT NULL,
-	`bank` INT(200) NULL DEFAULT NULL,
-    `cryto` INT(200) NULL DEFAULT NULL
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
+CREATE TABLE IF NOT EXISTS `money` (
+  `license` varchar(200) NOT NULL DEFAULT '0',
+  `cash` int(200) NOT NULL DEFAULT '0',
+  `bank` int(200) NOT NULL DEFAULT '0',
+  `cryto` int(200) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`license`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
